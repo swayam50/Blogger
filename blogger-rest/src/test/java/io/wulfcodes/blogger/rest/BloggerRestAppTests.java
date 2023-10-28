c2142d91111f11e71901594b5e51e69f603ff536
swayam50 <swayamsidh50@gmail.com>
2023-10-28 18:13:08 +0530

    added test classes

---
+++ blogger-rest/src/test/java/io/wulfcodes/blogger/rest/BloggerRestAppTests.java
@@ -0,0 +1,7 @@
+package io.wulfcodes.blogger.rest;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BloggerRestAppTests {
+}
---
+++ blogger-ui/src/test/java/io/wulfcodes/blogger/ui/BloggerUIAppTests.java
@@ -0,0 +1,7 @@
+package io.wulfcodes.blogger.ui;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BloggerUIAppTests {
+}
