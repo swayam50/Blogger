e4dabb4d8af8eafb4ccb25427c25871bf228f9d8
swayam50 <swayamsidh50@gmail.com>
2023-10-31 21:09:43 +0530

    changed to appropriate color names and partially done read post page

--- blogger-ui/src/main/webapp/src/pages/ReadPost.jsx
+++ blogger-ui/src/main/webapp/src/pages/ReadPost.jsx
@@ -2,7 +2,18 @@ import React from 'react';
 
 const ReadPost = () => {
     return (
-        <div>ReadPost</div>
+        <div className="read-post">
+            <div className="content">
+                <img src="https://images.pexels.com/photos/1000366/pexels-photo-1000366.jpeg" alt="post-img" />
+                <div className="user">
+                    <img src="https://images.pexels.com/photos/1000366/pexels-photo-1000366.jpeg" alt="user-img" />
+                    <div className="info">
+                        <span>John Doe</span>
+                        <p>Posted 2 days ago</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 };
 
